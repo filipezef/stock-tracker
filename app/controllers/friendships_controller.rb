79636c1dfe1da919a26c3f1c20451c3bca1d33fa
Filipe Campos Zeferino <filipezef@gmail.com>
2022-03-24 05:46:08 -0300

    friendships controller create and destroy methods set, methods integrated with views

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
+    current_user.friends << User.find(params[:friend])
+    flash[:notice] = "You are now following #{User.find(params[:friend]).full_name}"
+    redirect_to my_friends_path
   end
 
   def destroy
+    current_user.friendships.find_by(friend_id: params[:id]).destroy
+    flash[:notice] = "You are not following #{User.find(params[:id]).full_name}"
+    redirect_to my_friends_path
   end
 end
